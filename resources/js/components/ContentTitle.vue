5603f289a948b6385399dfa6c5fcd66a0881a716
kambe <50097614+nobuaki0331@users.noreply.github.com>
2020-08-08 13:06:13 +0900

    [add] スロット付きスコープの値渡しの再確認がしたかったので、あえてファイルを作成して使ってみました。

---
+++ resources/js/components/ContentTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot :data="$data" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sam',
+  data() {
+    return {
+      title1: '商品の口コミを投稿する',
+      title2: '新着商品',
+      title3: '口コミ一覧',
+      title4: 'お気に入りMOVIE',
+    }
+  }
+}
+</script>
